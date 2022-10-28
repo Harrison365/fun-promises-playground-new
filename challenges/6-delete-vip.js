38f5640e631c3513b6f652fba0c4f972d1b62a88
HannahBurks <hannahburks@hotmail.com>
2022-10-28 16:17:51 +0100

    This code will start off the code for removing a given person from the vip list

--- challenges/6-delete-vip.js
+++ challenges/6-delete-vip.js
@@ -1,11 +1,17 @@
 const fs = require("fs/promises");
 const inquirer = require("inquirer");
 
+
+const globalArr = []
+
 fs.readFile("./vip-list.txt", "utf8")
   .then((contents) => {
     return contents.split("\n");
   })
   .then((arr) => {
+   arr.forEach((person) => {
+   globalArr.push(person)
+})
     return inquirer.prompt([
       {
         type: "list",
@@ -16,7 +22,12 @@ fs.readFile("./vip-list.txt", "utf8")
     ]);
   })
   .then((answer) => {
-    console.log(answer);
+   const shorterArr = globalArr.filter((person) => {
+      return person !== answer.selection
+    })
+    const shorterStr = shorterArr.join("\n")
+    console.log(shorterStr)
+    // fs.writeFile("/vip-list.txt", shorterArr
   })
   .catch((err) => {
     console.log(err);
